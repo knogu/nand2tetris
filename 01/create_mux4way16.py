b4be80e6bddb790fe4c910482b35865a9590f8b7
knogu <kotaronoguchi61@gmail.com>
2021-07-26 13:36:37 +0900

    finished ch1

---
+++ 01/create_mux4way16.py
@@ -0,0 +1,3 @@
+# Mux4Way(a = a[0], b = b[0], c = c[0], d = d[0], sel = sel[2], out = out[0])
+for i in range(16):
+    print("Mux4Way(a = a[{}], b = b[{}], c = c[{}], d = d[{}], sel = sel, out = out[{}]);".format(i, i, i, i, i))
