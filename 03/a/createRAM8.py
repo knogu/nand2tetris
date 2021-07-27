da0682fa831a9099c2dcd7480af3d46ebec41fe3
knogu <kotaronoguchi61@gmail.com>
2021-07-27 19:12:22 +0900

    finished 03/a

---
+++ 03/a/createRAM8.py
@@ -0,0 +1,2 @@
+for i in range(8):
+    print("Register(in = in, load = load, out = memory[{}..{}]);".format(16*i,16*i+15))
---
+++ 03/a/create_register.py
@@ -0,0 +1,2 @@
+for i in range(16):
+    print("Bit(in = in[{}], load = load, out = out[{}]);".format(i,i))
