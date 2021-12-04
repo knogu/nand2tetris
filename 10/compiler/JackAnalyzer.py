454dd4f2c18354681c6f3e2d5c70708bcdcfad91
knogu <kotaronoguchi61@gmail.com>
2021-12-04 09:08:25 +0900

    トークン分割の実装

---
+++ 10/compiler/JackAnalyzer.py
@@ -0,0 +1,7 @@
+from JackTokenizer import JackTokenizer
+import sys
+
+path = sys.argv[1]
+tokenizer = JackTokenizer(path)
+# with open(path[:-4] + "xml", mode='w') as f:
+
---
+++ 10/compiler/JackTokenizer.py
@@ -0,0 +1,75 @@
+import re
+
+class JackTokenizer:
+    SYMBOLS = {"{", "}", "(", ")", "[", "]", ".", ",", ";", "+", "-", "*", "/", "&", "|", "<", ">", "=", "~"}
+
+    def __init__(self, file):
+        with open(file) as f:
+            s = f.read()
+        s = self.remove_comments(s)
+        s = re.sub('(^|(?<=\n))\t*\n', '', s)
+        s = re.sub('\t', '', s)
+        print("comments and empty line removed")
+        print(s)
+        self.s = s
+        self.seperate()
+    
+    def seperate(self):
+        self.tokens = self.s.split("\n")
+        self.tokens = self.serparate_str()
+        self.tokens = self.separete_by_space()
+        self.tokens = self.seperate_by_symbols()
+        for token in self.tokens:
+            # if not token:
+            #     print("this token is empty")
+            print(token)
+
+    def is_str(self, s):
+        return s[0] == s[-1] == "\""
+
+    def serparate_str(self):
+        new = []
+        for token in self.tokens:
+            groups = token.split("\"")
+            for i, group in enumerate(groups):
+                if i % 2 == 0:
+                    new.append(group)
+                else:
+                    new.append("\"" + group + "\"")
+        return new
+
+    def separete_by_space(self):
+        new = []
+        for token in self.tokens:
+            if len(token) == 0:
+                continue
+            if self.is_str(token):
+                new.append(token)
+                continue
+            groups = token.split(" ")
+            for group in groups:
+                if len(group) > 0:
+                    new.append(group)
+        return new
+    
+    def seperate_by_symbols(self):
+        new = []
+        for token in self.tokens:
+            word = []
+            for s in token:
+                if s in self.SYMBOLS:
+                    if word:
+                        new.append("".join(word))
+                        word = []
+                    new.append(s)
+                else:
+                    word.append(s)
+            if word:
+                new.append("".join(word))
+        return new
+
+    def remove_comments(self, s):
+        s = re.sub('/\*.*\*/', '', s)
+        pattern = re.compile(r'(?:^|(?<=\n))(.*)//.*(?:$|(?=\n))')
+        s = pattern.sub(r'\1', s)
+        return s
